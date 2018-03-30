9279724b7ab20cca79457d6f87d3ebf984eee98b
ObserverMoment <bosonsoul@gmail.com>
2018-03-30 22:24:58 +0100

    Commit before rails backend mods

--- src/components/IdeaTile.js
+++ src/components/IdeaTile.js
@@ -13,11 +13,11 @@ const Tile = styled.div`
   min-height: 180px;
   margin: 10px;
   border: ${props => props.isEditing
-        ? '3px solid lightblue;'
-        : '1px solid lightgrey;'}
-    box-shadow: ${props => props.isEditing
-        ? '0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);'
-        : '0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);'}
+      ? '3px solid lightblue;'
+      : '1px solid lightgrey;'}
+  box-shadow: ${props => props.isEditing
+      ? '0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);'
+      : '0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);'}
 `
 
 const DeleteIdeaIcon = styled.img`
